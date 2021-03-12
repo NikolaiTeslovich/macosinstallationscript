970ccb0d4ab41957db3ed28cd89cbafb20fed9dd
Nikolai Nekrutenko <nekrutnikolai@gmail.com>
2021-03-11 22:37:37 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+## Apps that I use regularly on my mac
+
+# First install command line Xcode tools
+xcode-select --install
+
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install the packages that I use
+brew install git
+
+# install all the apps
+brew install --cask \
+adobe-creative-cloud google-chrome iterm2 appcleaner atom discord exodus expressvpn ledger-live pock skype spotify transmission zoom
